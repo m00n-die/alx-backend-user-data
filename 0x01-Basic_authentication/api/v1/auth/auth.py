f32514ac4678d05d6371c68c79567a909584e377
m00n-die <reginaldrock219@gmail.com>
2024-04-16 18:06:30 +0100

    task 03

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,18 @@ class Auth():
     """The authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth function"""
-        return False
+        path = str(path)
+        path = path.rstrip("/")
+        excluded_paths = [ep.rstrip("/") for ep in excluded_paths] \
+            if excluded_paths else []
+
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for ex_path in excluded_paths:
+            if path == ex_path:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """function that adds authorization header"""
